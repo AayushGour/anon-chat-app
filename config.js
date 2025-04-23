2fea6e982f846d41fc18ffbd912d84dd6465fd8f
Aayush Gour <aayush.gour@cognida.ai>
2025-04-24 00:01:42 +0530

    updated env vars

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const REACT_APP_API_URL = "https://anon-chat-app-lmgs.onrender.com/"
+const REACT_APP_API_URL = "https://anon-chat-app-lmgs.onrender.com"
 // const REACT_APP_API_URL = "http://localhost:3000"
